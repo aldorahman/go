2f61ce36addd99722568f4a4602c454d853a0abe
aldorahman <rahmanaldo69@gmail.com>
2022-05-19 18:34:10 +0700

    m

--- main.go
+++ main.go
@@ -3,10 +3,17 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
 	http.Handle("/", http.FileServer(http.Dir("./static")))
 	log.Println("listening on port 3000...")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":"+port, nil)
 }
